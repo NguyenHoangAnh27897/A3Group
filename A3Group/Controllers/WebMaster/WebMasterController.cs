a36450e04c829898c13b2630039269fdf533bbdb
Hoang Anh <hoananh27897@gmail.com>
2018-11-24 16:24:27 +0700

    Khoi tao du an

---
+++ A3Group/Controllers/WebMaster/WebMasterController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace A3Group.Controllers.WebMaster
+{
+    public class WebMasterController : Controller
+    {
+        // GET: WebMaster
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public ActionResult
+    }
+}
